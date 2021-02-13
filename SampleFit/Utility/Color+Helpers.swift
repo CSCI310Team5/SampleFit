889cfd8db28c01c88b70f2b58959cac36b9c9e28
Zihan Qi <qizihan0428@foxmail.com>
2021-02-13 00:33:48 -0800

    Utility Addition

---
+++ SampleFit/Utility/Color+Helpers.swift
@@ -0,0 +1,24 @@
+//
+//  Color+Helpers.swift
+//  SampleFit
+//
+//  Created by Zihan Qi on 2/12/21.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+extension Color {
+    static var systemBackground: Color {
+        return Color(UIColor.systemBackground)
+    }
+    static var systemFill: Color {
+        return Color(UIColor.systemFill)
+    }
+    static var systemBlue: Color {
+        return Color(UIColor.systemBlue)
+    }
+}
+
+
