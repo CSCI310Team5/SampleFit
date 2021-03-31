8725077b6340355f76918e289986ad1c8bbba506
Zihan Qi <qizihan0428@foxmail.com>
2021-03-30 22:31:00 -0700

    removed test launch performance

--- SampleFitUITests/RegisterSignInUITests.swift
+++ SampleFitUITests/RegisterSignInUITests.swift
@@ -155,16 +155,4 @@ class RegisterSignInUITests: XCTestCase {
         
         XCTAssertTrue(app.staticTexts["Me"].exists)
     }
-    
-    
-
-    
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
